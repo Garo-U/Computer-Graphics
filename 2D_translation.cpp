714f45c756e958da646f94eb9d72f66246e6c558
Garo-U <112748702+Garo-U@users.noreply.github.com>
2022-12-27 20:00:37 +0530

    Update 2D_translation.cpp

--- 2D_translation.cpp
+++ 2D_translation.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int gd=DETECT,gm,s;
     initgraph(&gd,&gm,(char*)"");
-    cout<<"1.Translation\n2.Rotation\n3.Scaling\n4.Reflection\n5.Shearing   "<<endl;
+    cout<<"1.Translation\n2.Rotation\n3.Scaling : "<<endl;
     cout<<"Selection:";
     cin>>s;
     switch(s)
@@ -51,41 +51,7 @@ int main()
                 getch();
                 break;
 }
-        case 4:
-            {
-                 int x1=200,y1=300,x2=500,y2=300,x3=350,y3=400;
-                 cout<<"triangle before reflection"<<endl;
-                 setcolor(3);
-                 line(x1,y1,x2,y2);
-                 line(x1,y1,x3,y3);
-                 line(x2,y2,x3,y3);
-cout<<"triangle after reflection"<<endl;
-                 setcolor(5);
-                 line(x1,-y1+500,x2,-y2+500);
-                 line(x1,-y1+500,x3,-y3+500);
-                 line(x2,-y2+500,x3,-y3+500);
-                 getch();
-                 break;
-}
-        case 5:
-        {
-  int x1=400,y1=100,x2=600,y2=100,x3=400,y3=200,x4=600,y4=200,shx=2;
-             cout<<"Before shearing of rectangle"<<endl;
-             setcolor(3);
-             line(x1,y1,x2,y2);
-             line(x1,y1,x3,y3);
-             line(x3,y3,x4,y4);
-             line(x2,y2,x4,y4);
-             cout<<"After shearing of rectangle"<<endl;
-             x1=x1+shx*y1;
-             x2=x2+shx*y2;
-             x3=x3+shx*y3;
-             x4=x4+shx*y4;
-             setcolor(13);
-             line(x1,y1,x2,y2);
-             line(x1,y1,x3,y3);
-             line(x3,y3,x4,y4);
-             line(x2,y2,x4,y4);
+       
 getch();
 }
 default:
